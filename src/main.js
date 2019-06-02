371d37f2e7063c57c5570efc4c5e682a53ce40bd
yuliangwang <wyl0706@gmail.com>
2019-06-02 21:46:06 +0800

    create HomePage

--- src/main.js
+++ src/main.js
@@ -3,12 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import fastclick from 'fastclick'
-import './assets/styles/reset.css'
-import './assets/styles/border.css'
+import fastClick from 'fastclick'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
-fastclick.attach(document.body)
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
