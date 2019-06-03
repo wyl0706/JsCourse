dbaa3d738f7f37666bf798b0ef3b5b7b7084c9c1
yuliangwang <wyl0706@gmail.com>
2019-06-03 23:27:45 +0800

    create HomeIcons

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,15 +2,18 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <HomeIcons></HomeIcons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './compoments/Header'
 import HomeSwiper from './compoments/Swiper'
+import HomeIcons from './compoments/Icons'
 export default {
   name: 'Home',
   components: {
+    HomeIcons,
     HomeSwiper,
     HomeHeader
   }
--- src/pages/home/compoments/Swiper.vue
+++ src/pages/home/compoments/Swiper.vue
@@ -41,8 +41,7 @@ export default {
     overflow hidden
     width 100%
     height 0
-    padding-bottom 31.25%
-
+    padding-bottom 26.67%
     .swiper-img
       width 100%
 </style>
