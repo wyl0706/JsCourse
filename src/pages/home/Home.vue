d24b4f38557014447cd52b02218c4d1d91ad1909
yuliangwang <wyl0706@gmail.com>
2019-06-30 01:34:53 +0800

    add 红心

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,6 +3,7 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <HomeIcons></HomeIcons>
+    <home-recommend></home-recommend>
   </div>
 </template>
 
@@ -10,12 +11,14 @@
 import HomeHeader from './compoments/Header'
 import HomeSwiper from './compoments/Swiper'
 import HomeIcons from './compoments/Icons'
+import HomeRecommend from './compoments/Recommend'
 export default {
   name: 'Home',
   components: {
     HomeIcons,
     HomeSwiper,
-    HomeHeader
+    HomeHeader,
+    HomeRecommend
   }
 }
 </script>
---
+++ src/pages/home/compoments/Recommend.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <div class="recommend-title">
+      <span class="iconfont">&#xe85c;</span>
+      猜你喜欢
+    </div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img class="item-img" :src="item.imgUrl" alt="img">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
+        title: '上海迪士尼乐园',
+        desc: '上海迪士尼乐园上海迪士尼乐园上海迪士尼乐园上海迪士尼乐园'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
+        title: '上海迪士尼乐园',
+        desc: '上海迪士尼乐园上海迪士尼乐园上海迪士尼乐园上海迪士尼乐园'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
+        title: '上海迪士尼乐园',
+        desc: '上海迪士尼乐园上海迪士尼乐园上海迪士尼乐园上海迪士尼乐园'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
+        title: '上海迪士尼乐园',
+        desc: '上海迪士尼乐园上海迪士尼乐园上海迪士尼乐园上海迪士尼乐园'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/minins.styl"
+  .recommend-title
+    margin-top .2rem
+    line-height .8rem
+    background #eee
+    text-indent .2rem
+
+  .item
+    overflow hidden
+    display flex
+    height 1.9rem
+    .item-img
+      width 1.7rem
+      height 1.7rem
+      padding .1rem
+    .item-info
+      flex 1
+      padding .1rem
+      min-width 0
+      .item-title
+        line-height .54rem
+        font-size .32rem
+        ellipse()
+      .item-desc
+        line-height .4rem
+        color  #ccc
+        ellipse()
+      .item-button
+        line-height .44rem
+        margin-top .16rem
+        background #ff9300
+        padding 0 .1rem
+        border-radius .06rem
+        color #fff
+  .iconfont
+    color red
+</style>
